883eee5e28abd692461f77ff8c1538074da91641
Jhon Sanchez <jhonhst2@gmail.com>
2023-03-10 08:49:56 -0500

    Updates

---
+++ precache-manifest.384e911f570e2167241224917369d095.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a67e4cf06c5ff6982405",
+    "url": "/pdf-viewer/static/css/main.ff3904b5.chunk.css"
+  },
+  {
+    "revision": "a67e4cf06c5ff6982405",
+    "url": "/pdf-viewer/static/js/main.a67e4cf0.chunk.js"
+  },
+  {
+    "revision": "33129f50c507051f4f8e",
+    "url": "/pdf-viewer/static/js/1.33129f50.chunk.js"
+  },
+  {
+    "revision": "09b49ca14b213aed40a9",
+    "url": "/pdf-viewer/static/js/runtime~main.09b49ca1.js"
+  },
+  {
+    "revision": "4fc459b4dfd9e54f3852c99ebb0ce7d3",
+    "url": "/pdf-viewer/index.html"
+  }
+];
